b0e358a11a505d816be100e37bef43c46c41e611
omri-gendler <foromri.g@gmail.com>
2025-07-08 20:47:03 +0300

    update css for filter section

--- apps/mail/cmps/MailFilter.jsx
+++ apps/mail/cmps/MailFilter.jsx
@@ -32,8 +32,8 @@ export function MailFilter({ filterBy, onSetFilterBy, mails }) {
     return (
         <section>
             <div className="search-bar-container">
-
                 <svg className="search-icon" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" fill="black"><path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z" /></svg>
+
                 <input className="search-input"
 
                     type="text"
@@ -44,19 +44,18 @@ export function MailFilter({ filterBy, onSetFilterBy, mails }) {
                 />
             </div>
 
-            <section className="filter-section">
-                <input className="filter-isRead"
-                    list="isRead"
-                    name="isRead"
-                    onChange={handleChange}
-                />
-                <datalist id="isRead">
-                    <option value="all"></option>
-                    <option value="read"></option>
-                    <option value="unread"></option>
-                </datalist>
-            </section>
+            <select className="isread-filter"
 
+                name="isRead"
+                onChange={handleChange}
+                defaultValue="all"
+            >
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="black"><path d="M480-240 240-480l56-56 144 144v-368h80v368l144-144 56 56-240 240Z" /></svg>
+
+                <option value="all">All</option>
+                <option value="read">Read</option>
+                <option value="unread">Unread</option>
+            </select>
         </section>
     )
 }
